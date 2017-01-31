f99b9103d91362667b726f126c6e4252652b33df
Dimitry Robin <dimy@hotmail.fr>
2017-01-31 16:30:40 +0100

    Update of EmployeDAOImplTest

--- myrhdao/src/test/java/fr/imie/tp/myrh/dao/impl/EmployeDAOImplTest.java
+++ myrhdao/src/test/java/fr/imie/tp/myrh/dao/impl/EmployeDAOImplTest.java
@@ -1,31 +1,44 @@
 package fr.imie.tp.myrh.dao.impl;
 
+import static org.junit.Assert.*;
+
+import java.util.List;
+
 import fr.imie.tp.myrh.dao.ifc.IEmployeDAO;
+import fr.imie.tp.myrh.dao.model.Employe;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
- * Created by ndasilva on 31/01/2017.
+ * Created by drobin on 31/01/2017.
  */
 public class EmployeDAOImplTest {
-    IEmployeDAO _employeDaoService = new EmployeDAOimpl();
+    // static IEmployeDAO _employeDaoService = new EmployeDAOimpl();
+
+    static IEmployeDAO _employeDaoService; 
 
     public static void main(String[] args) {
-
+    	
     }
-    @BeforeClass
-    public static void setUpBeforeClass() throws Exception{
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		_employeDaoService = new EmployeDAOimpl();
+	}
 
-    }
     @AfterClass
     public static void tearDownAfterClass() throws Exception{
 
     }
-    @Test
-    public void testGetAllEmploye(){
-
-    }
-
+	
+	@Test
+	public void testGetAllEmploye() {
+		List<Employe> emps = (List<Employe>)_employeDaoService.getAllEmploye();
+		assertNotNull(emps);
+		assertEquals(7, emps.size());
+		assertEquals("Prenom incorrect", "Gertrude", emps.get(0).getPrenom());
+	}
 
 }
